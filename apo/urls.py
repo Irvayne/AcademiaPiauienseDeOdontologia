bfc4da81525b46ad274d4df87219d856355e01fd
Irvayne Ibiapina <irvayne@MacBook-Air-de-Irvayne.local>
2018-09-20 13:21:56 -0300

    Crud de Regimento e tela de criacao de regimento

--- apo/urls.py
+++ apo/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.conf.urls import url, include
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
     url('', include('front.urls')),
+    url('', include('back.urls')),
 ]
--- front/urls.py
+++ front/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib import admin
 from django.conf.urls import url
 from front import views
 
