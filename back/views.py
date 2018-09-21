b651cb00b080a122329aa08147324445d19143d7
Irvayne Ibiapina <irvayne@MacBook-Air-de-Irvayne.local>
2018-09-21 15:43:36 -0300

    adicao do cadastro e listagem das funcinalidades

--- back/views.py
+++ back/views.py
@@ -1,25 +1,25 @@
 from django.shortcuts import render, redirect
-from core.models import Estatuto, Noticia
+from core.models import Estatuto, Noticia, Diretoria, Membro, Curso, Cadeira, Sobre
 
 def home(request):
 	return render(request, 'home.html')
 
 def estatuto(request):
-	return render(request, 'estatuto.html', {'estatuto': Estatuto.objects.get(id=1) })
+	try:
+		estatuto = Estatuto.objects.get(id=1)
+	except:
+		estatuto = Estatuto(conteudo='Digite o conteudo do estatuto')	
+		estatuto.save()
+	return render(request, 'estatuto.html', {'estatuto': estatuto})
 
 def estatuto_form(request):
+	estatuto = Estatuto.objects.get(id=1)
 	if request.method == 'POST':
-		
-		estatuto = Estatuto.objects.get(id=1)
 		estatuto.conteudo = request.POST['estatuto_conteudo']
 		estatuto.save()
-		return render(request, 'estatuto.html',  {'estatuto': Estatuto.objects.get(id=1)})
+		return redirect('estatuto')
 	else:
-		estatuto = Estatuto.objects.get(id=1)
-		#estatuto = Estatuto(conteudo='Digite o conteudo do estatuto')
-		
-
-	return render(request, 'estatuto_form.html', {'estatuto': estatuto })
+		return render(request, 'estatuto_form.html', {'estatuto': estatuto })
 
 
 def noticias(request):
@@ -32,3 +32,83 @@ def noticias_form(request):
 		return redirect('noticias')
 	else:
 		return render(request, 'noticias_form.html')
+
+def diretorias(request):
+	try:
+		diretorias = Diretoria.objects.all()
+	except:
+		diretorias = []
+	return render(request, 'diretorias.html', {'diretorias': diretorias})
+
+def diretorias_form(request):
+	if request.method == 'POST':
+		diretoria = Diretoria(nome=request.POST['nome'], descricao=request.POST['descricao'])
+		diretoria.save()
+		return redirect('diretorias')
+	else:
+		return render(request, 'diretorias_form.html')
+
+def membros(request):
+	try:
+		membros = Membro.objects.all()
+	except:
+		membros = []
+	return render(request, 'membros.html', {'membros': membros})
+
+def membros_form(request):
+	if request.method == 'POST':
+		membro = Membro(nome=request.POST['nome'], descricao=request.POST['descricao'])
+		membro.save()
+		return redirect('membros')
+	else:
+		return render(request, 'membros_form.html')
+
+def cursos(request):
+	try:
+		cursos = Curso.objects.all()
+	except:
+		cursos = []
+	return render(request, 'cursos.html', {'cursos': cursos})
+
+def cursos_form(request):
+	if request.method == 'POST':
+		curso = Curso(nome=request.POST['nome'], descricao=request.POST['descricao'], conteudo=request.POST['conteudo'])
+		curso.save()
+		return redirect('cursos')
+	else:
+		return render(request, 'cursos_form.html')
+
+def cadeiras(request):
+	try:
+		cadeiras = Cadeira.objects.all()
+	except:
+		cadeiras = []
+	return render(request, 'cadeiras.html', {'cadeiras': cadeiras})
+
+def cadeiras_form(request):
+	if request.method == 'POST':
+		cadeira = Cadeira(nome=request.POST['nome'], descricao=request.POST['descricao'])
+		cadeira.save()
+		return redirect('cadeiras')
+	else:
+		return render(request, 'cadeiras_form.html')
+
+def sobre(request):
+	try:
+		sobre = Sobre.objects.get(id=1)
+	except:
+		sobre = Sobre(conteudo="")
+		sobre.save()
+
+	return render(request, 'sobre.html', {'sobre': sobre })
+
+def sobre_form(request):
+	sobre = Sobre.objects.get(id=1)
+	if request.method == 'POST':
+		sobre.conteudo = request.POST['conteudo']
+		sobre.save()
+		return redirect('sobre')
+	else: 	
+		return render(request, 'sobre_form.html', {'sobre': sobre })
+
+
--- core/models.py
+++ core/models.py
@@ -10,3 +10,30 @@ class Noticia(models.Model):
 	titulo = models.CharField(max_length=1000)
 	subtitulo = models.CharField(max_length=1000)
 	conteudo = models.CharField(max_length=40000)
+
+class Diretoria(models.Model):
+	
+	nome = models.CharField(max_length=1000)
+	descricao = models.CharField(max_length=1000)
+
+class Membro(models.Model):
+
+	nome = models.CharField(max_length=1000)
+	descricao = models.CharField(max_length=1000)
+
+class Curso(models.Model):
+
+	nome = models.CharField(max_length=1000)
+	descricao = models.CharField(max_length=1000)
+	conteudo = models.CharField(max_length=1000)
+
+class Cadeira(models.Model):
+
+	nome = models.CharField(max_length=1000)
+	descricao = models.CharField(max_length=1000)
+
+class Sobre(models.Model):
+
+	conteudo = models.CharField(max_length=1000)
+	
+		
