2845d58f19523dba95f36f50d4230797c83c782f
Alexandre Lages <alexandrelages64@gmail.com>
2018-09-21 17:38:25 -0300

    Linkagem dos objetos criados com o index.html

--- core/models.py
+++ core/models.py
@@ -1,39 +1,35 @@
 from django.db import models
 
 class Estatuto(models.Model):
-
 	conteudo = models.CharField(max_length=1000)
 
 
 class Noticia(models.Model):
-
 	titulo = models.CharField(max_length=1000)
 	subtitulo = models.CharField(max_length=1000)
 	conteudo = models.CharField(max_length=40000)
 
+
 class Diretoria(models.Model):
-	
 	nome = models.CharField(max_length=1000)
 	descricao = models.CharField(max_length=1000)
+
 
 class Membro(models.Model):
-
 	nome = models.CharField(max_length=1000)
 	descricao = models.CharField(max_length=1000)
+
 
 class Curso(models.Model):
-
 	nome = models.CharField(max_length=1000)
 	descricao = models.CharField(max_length=1000)
 	conteudo = models.CharField(max_length=1000)
+
 
 class Cadeira(models.Model):
-
 	nome = models.CharField(max_length=1000)
 	descricao = models.CharField(max_length=1000)
 
-class Sobre(models.Model):
 
+class Sobre(models.Model):
 	conteudo = models.CharField(max_length=1000)
-	
-		
--- front/views.py
+++ front/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
-from core.models import Estatuto
+from core.models import Estatuto, Sobre, Noticia, Curso
 
 def index(request):
-	return render(request, 'index.html')
+	noticias = Noticia.objects.all()
+	cursos = Curso.objects.all()
+
+	if len(noticias) >= 3:
+		ultimas_noticias = noticias[len(noticias) - 3 : len(noticias)]
+	elif len(noticias) >= 2:
+		ultimas_noticias = noticias[len(noticias) - 2 : len(noticias)]
+	else:
+		ultimas_noticias = noticias
+
+	if len(cursos) >= 3:
+		ultimos_cursos = cursos[len(cursos) - 3 : len(cursos)]
+	elif len(cursos) >= 2:
+		ultimos_cursos = cursos[len(cursos) - 2 : len(cursos)]
+	else:
+		ultimos_cursos = cursos
+		
+	sobre = Sobre.objects.get(id=1)
+	return render(request, 'index.html', {'sobre': sobre, 'ultimas_noticias': ultimas_noticias, 'ultimos_cursos': ultimos_cursos})
 
 def estatuto(request):
 	estatuto = Estatuto.objects.get(id=1)
