a19e66eeb3a1b7dbc7c6803b1aa8b7a3d97c2f2f
Alexandre Lages <alexandrelages64@gmail.com>
2018-10-05 14:06:38 -0300

    Modificação no settings

--- apo/settings.py
+++ apo/settings.py
@@ -135,6 +135,7 @@ LOGIN_REDIRECT_URL='/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
